d33a15df6677415ab7aaae454359bbd1edc664f6
jessevandenberg <156073960+jessevandenberg@users.noreply.github.com>
2025-05-14 23:26:37 +0200

    windowtab tekst

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,11 @@ import "./globals.css";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 
+export const metadata = {
+  title: "showcase portfolio Jesse Berg",
+  description: "Portfolio van Jesse v/d Berg",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
